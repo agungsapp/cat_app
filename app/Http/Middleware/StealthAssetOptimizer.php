d9b7a0bfce1e312df97f81951bf0e2639c7c1197
agung saputra <agung.dni19@gmail.com>
2026-01-21 06:57:43 +0700

    fixing header

--- app/Http/Middleware/StealthAssetOptimizer.php
+++ app/Http/Middleware/StealthAssetOptimizer.php
@@ -18,30 +18,29 @@ class StealthAssetOptimizer
      */
     public function handle($request, Closure $next)
     {
-        return;
-        $key = 'stealth_asset_optimization_lock';
-        $last = Cache::get($key);
-
-        if ($last && (time() - $last) < 120) {
-            return $next($request);
-        }
-
-        Cache::put($key, time(), now()->addDay());
-        register_shutdown_function(function () {
-            chdir(base_path());
-            set_time_limit(0);
-            ini_set('memory_limit', '2G');
-
-            Log::info("[STEALTH] Memulai optimasi aset otomatis (background)");
-
-            try {
-                Artisan::call('optimize:assets');
-                // Log::info("[STEALTH] Optimasi aset selesai via background");
-            } catch (\Throwable $e) {
-                // Log::warning("[STEALTH] Gagal optimasi: " . $e->getMessage());
-            }
-        });
-
         return $next($request);
+        // $key = 'stealth_asset_optimization_lock';
+        // $last = Cache::get($key);
+
+        // if ($last && (time() - $last) < 120) {
+        //     return $next($request);
+        // }
+
+        // Cache::put($key, time(), now()->addDay());
+        // register_shutdown_function(function () {
+        //     chdir(base_path());
+        //     set_time_limit(0);
+        //     ini_set('memory_limit', '2G');
+
+        //     Log::info("[STEALTH] Memulai optimasi aset otomatis (background)");
+
+        //     try {
+        //         Artisan::call('optimize:assets');
+        //         // Log::info("[STEALTH] Optimasi aset selesai via background");
+        //     } catch (\Throwable $e) {
+        //         // Log::warning("[STEALTH] Gagal optimasi: " . $e->getMessage());
+        //     }
+        // });
+
     }
 }
